b0f757fb15bd95288d96de2f7a04ed6cf9aab2e2
jaizyy <jaizyzz2@gmail.com>
2025-05-12 13:26:24 +0200

    new

--- script.js
+++ script.js
@@ -1,20 +1,8 @@
-document.getElementById('login-form').addEventListener('submit', function(e) {
-  e.preventDefault();
-
-  const email = document.getElementById('email').value;
-  const ageCheck = document.getElementById('age-check').checked;
-
-  if (email && ageCheck) {
-    document.getElementById('login-section').classList.add('hidden');
-    document.getElementById('gallery-section').classList.remove('hidden');
-  } else {
-    alert("Veuillez entrer un e-mail valide et confirmer que vous avez plus de 18 ans.");
-  }
-});
-
-// Supprimer le flou au clic
-document.addEventListener('click', function(e) {
-  if (e.target.classList.contains('blurred')) {
-    e.target.classList.remove('blurred');
-  }
+document.querySelectorAll('.item img, .item video').forEach(element => {
+  element.addEventListener('click', function() {
+    const link = this.getAttribute('data-link');
+    if (link) {
+      window.location.href = link;
+    }
+  });
 });
